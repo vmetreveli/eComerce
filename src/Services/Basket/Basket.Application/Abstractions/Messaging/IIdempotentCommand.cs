8be338a13e4d4e446751a91c53488847469a47ae
Vakhushti Metreveli <vakhushti.metreveli@gmail.com>
2022-06-22 11:19:44 +0400

    commit

--- src/Services/Basket/Basket.Application/Abstractions/Messaging/IIdempotentCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Basket.Application.Abstractions.Messaging;
-
-public interface IIdempotentCommand<out TResponse> : ICommand<TResponse>
-{
-    Guid RequestId { get; set; }
-}
