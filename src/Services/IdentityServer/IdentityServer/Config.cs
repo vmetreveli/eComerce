9b2ab9bf7d5e45972ec29399ab932c2cd238ecce
Vakhushti Metreveli <vakhushti.metreveli@gmail.com>
2022-07-01 10:19:16 +0400

    commit

---
+++ src/Services/IdentityServer/IdentityServer/Config.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System.Collections.Generic;
+using IdentityServer4;
+using IdentityServer4.Models;
+namespace IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<ApiResource> GetApiResources() =>
+        new[]
+        {
+            new ApiResource("HandbookAPI")
+            {
+                Scopes = new List<string>
+                {
+                    "HandbookAPI.read",
+                    "HandbookAPI.write",
+                    "HandbookAPI"
+                }
+                // ApiSecrets = new List<Secret>
+                // {
+                //     new Secret("ClientSecret1".Sha256())
+                // },
+                // UserClaims = new List<string>
+                // {
+                //     "role"
+                // }
+            }
+
+        };
+
+    public static IEnumerable<IdentityResource> GetIdentityResources() =>
+        new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new()
+            {
+                Name = "role",
+                UserClaims = new List<string> { "role" }
+            }
+        };
+
+    public static IEnumerable<ApiScope> GetApiScopes() =>
+        new List<ApiScope>
+        {
+            new("HandbookAPI.read"),
+            new("HandbookAPI.write"),
+            new("HandbookAPI")
+        };
+
+
+    public static IEnumerable<Client> GetClients(string apiUrl) =>
+        new[]
+        {
+
+            new()
+            {
+                ClientId = "client_id_swagger",
+                ClientSecrets = { new Secret("client_secret_swagger".Sha256()) },
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                AllowedCorsOrigins = { $"{apiUrl}" },
+                AllowedScopes =
+                {
+                    "HandbookAPI",
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile
+                },
+
+                RequirePkce = true,
+                RequireConsent = true,
+                AllowPlainTextPkce = false,
+
+                RedirectUris = { $"{apiUrl}/swagger/oauth2-redirect.html" },
+
+                PostLogoutRedirectUris = { $"{apiUrl}/swagger/" }
+            },
+
+            new Client
+            {
+                ClientName = "Postman",
+                AllowOfflineAccess = true,
+                AllowedScopes =
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "roles",
+                    "HandbookAPI"
+                },
+                RedirectUris = new []
+                {
+                    "https://www.getpostman.com/oauth2/callback",
+
+                },
+                Enabled = true,
+                ClientId = "client_id_postman",
+                ClientSecrets = { new Secret("client_secret_postman".Sha256()) },
+                PostLogoutRedirectUris = { $"http://localhost:5000/signout-callback-oidc" },
+                ClientUri = null,
+                AllowedGrantTypes = new[]
+                {
+                    GrantType.ResourceOwnerPassword
+                }
+            }
+
+        };
+}
---
+++ src/Services/IdentityServer/IdentityServer/Startup.cs
@@ -0,0 +1,121 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+namespace IdentityServer;
+
+public class Startup
+{
+
+    public Startup(IWebHostEnvironment environment, IConfiguration configuration)
+    {
+        Environment = environment;
+        Configuration = configuration;
+    }
+    public IWebHostEnvironment Environment { get; }
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+
+        var connectionString = Configuration.GetConnectionString("DefaultConnection");
+        var assembly = typeof(Program).Assembly.GetName().Name;
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(connectionString, b => b.MigrationsAssembly(assembly)));
+
+        services.AddIdentity<IdentityUser, IdentityRole>(config =>
+            {
+                // config.Password.RequireDigit = false;
+                // config.Password.RequireLowercase = false;
+                // config.Password.RequireNonAlphanumeric = false;
+                // config.Password.RequireUppercase = false;
+                // config.Password.RequiredLength = 5;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        services.ConfigureApplicationCookie(config =>
+        {
+            //config.LoginPath = "/Auth/Login";
+            //config.LogoutPath = "/Auth/Logout";
+            config.Cookie.Name = "IdentityServer.Cookies";
+        });
+
+        services.AddIdentityServer(options =>
+            {
+                // options.Events.RaiseErrorEvents = true;
+                // options.Events.RaiseInformationEvents = true;
+                // options.Events.RaiseFailureEvents = true;
+                // options.Events.RaiseSuccessEvents = true;
+                //
+                // // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+                // options.EmitStaticAudienceClaim = true;
+            })
+            .AddAspNetIdentity<IdentityUser>()
+            //.AddTestUsers(TestUsers.Users)
+            // this adds the config data from DB (clients, resources, CORS)
+            .AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = builder => builder.UseNpgsql(connectionString, opt =>
+                    opt.MigrationsAssembly(assembly));
+            })
+            // this adds the operational data from DB (codes, tokens, consents)
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = builder => builder.UseNpgsql(connectionString, opt =>
+                    opt.MigrationsAssembly(assembly));
+
+                // this enables automatic token cleanup. this is optional.
+                options.EnableTokenCleanup = true;
+            })
+            // not recommended for production - you need to store your key material somewhere secure
+            .AddDeveloperSigningCredential();
+
+        services.AddAuthentication();
+
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        if (Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseDatabaseErrorPage();
+        }
+
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseIdentityServer();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapDefaultControllerRoute();
+        });
+    }
+    /*
+     dotnet  ef migrations add "InitialAspNetIdentityMigration"  --context   PersistedGrantDbContext
+     dotnet  ef database  update --context PersistedGrantDbContext
+
+     dotnet  ef migrations add "InitialAspNetIdentityMigration"  --context   ConfigurationDbContext
+     dotnet  ef database  update --context ConfigurationDbContext
+
+     dotnet  ef migrations add "InitialAspNetIdentityMigration"  --context   ApplicationDbContext
+     dotnet  ef database  update --context ApplicationDbContext
+     */
+
+    /*
+     * First Run From Terminal
+     * dotnet run /seed --project IdentityServer
+     */
+}
